caed4b911210efab0643cdfd5114677e4f508c25
Jure Cuhalev <jure@cuhalev.com>
2020-04-01 16:37:30 +0200

    Updates header title and favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `OER Policy Registry`,
+    title: `OE Policy Registry`,
     description: `Open Education Policy Registry`,
     author: `@hbz`,
   },
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/svgs/icon-globe.svg`, // This path is relative to the root of the site.
       },
     },
     {
