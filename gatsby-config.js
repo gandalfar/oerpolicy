a7619d09122770077776ca0eb69fa02b5b33c25e
Jure Cuhalev <jure@cuhalev.com>
2020-04-01 15:54:57 +0200

    Fixes tailwind purgecss build

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,11 +46,19 @@ module.exports = {
       options: {
         printRejected: false, // Print removed selectors and processed file names
         // develop: true, // Enable while using `gatsby develop`
-        // tailwind: true, // Enable tailwindcss support
-        whitelistPatterns: [/[\w-/.:]+(?<!:)/g],
+        tailwind: false, // Enable tailwindcss support
+        // whitelistPatterns: [/[\w-/.:]+(?<!:)/g, /[\w-/:\.]+(?<!:)/g],
         // whitelist: ['whitelist'], // Don't remove this selector
         // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+        extractors: [
+          {
+            extractor: content => {
+              return content.match(/[\w-/.:]+(?<!:)/g) || []
+            },
+            extensions: ["js", "ts", "jsx", "tsx", "md", "mdx"],
+          },
+        ],
       },
     },
 
