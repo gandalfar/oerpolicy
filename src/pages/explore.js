fe16ad25f315c7497875ab5ccc37780f9cab4ee1
Jure Cuhalev <jure@cuhalev.com>
2020-05-06 14:53:52 +0200

    Basic responsive chart

--- src/pages/explore.js
+++ src/pages/explore.js
@@ -9,13 +9,14 @@ import SEO from "../components/seo"
 import HeroSmall from "../components/hero-small"
 import ContentFeature from "../components/content-feature"
 import OneColumn from "../components/one-column"
+// import TwoColumns from "../components/two-columns"
+// import TextColumn from "../components/text-column"
 
 const ExplorePage = () => {
   const url =
     "https://oerworldmap.org/resource.json?q=about.@type:Policy&sort=dateCreated:DESC&size=500"
 
   const { data } = useSWR(url, fetcher)
-  console.log(data)
 
   return (
     <Layout
@@ -36,16 +37,21 @@ const ExplorePage = () => {
           description: "Discover all the different types of OE Policies",
         }}
       >
-        <OneColumn className="w-full lg:w-2/3">
-          <p>
+        <OneColumn className="w-full justify-center flex">
+          <p className="max-w-lg">
             OER World map currently tracks {data ? data.totalItems : "300+"}{" "}
             policies. This is a short extract of a few ways to look at the data
-            and make if more valuable for you.
+            so that it can support you in creating your policy.
           </p>
         </OneColumn>
-      </ContentFeature>
 
-      {!data ? <p>Loading data ...</p> : <PieChart items={data.member} />}
+        <OneColumn className="w-full lg:w-2/3">
+          <h3 className="text-2xl font-bold text-center">
+            Open Education Policies by Focus
+          </h3>
+          {!data ? <p>Loading data ...</p> : <PieChart items={data.member} />}
+        </OneColumn>
+      </ContentFeature>
 
       <SEO title="Explore OE Policies" />
     </Layout>
@@ -60,23 +66,30 @@ const PieChart = ({ items }) => {
   )
 
   return (
-    <Plot
-      data={[
-        {
-          values: Object.values(data),
-          labels: Object.keys(data),
-          type: "pie",
-          automargin: true,
-          sort: true,
-        },
-      ]}
-      layout={{
-        width: "auto",
-        height: 500,
-        title: "Avaliable policies by focus",
-      }}
-      config={{ displaylogo: false }}
-    />
+    <div className="w-full">
+      <Plot
+        style={{ width: "100%", "min-height": "500px" }}
+        data={[
+          {
+            values: Object.values(data),
+            labels: Object.keys(data),
+            type: "pie",
+            automargin: true,
+            sort: true,
+            showlegend: true,
+            textinfo: "text",
+            textposition: "inside",
+            texttemplate: "%{percent:1%f}",
+            mode: "text",
+            hole: 0.7,
+          },
+        ]}
+        layout={{
+          autosize: true,
+        }}
+        config={{ displaylogo: false, responsive: true }}
+      />
+    </div>
   )
 }
 
